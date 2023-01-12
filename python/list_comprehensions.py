8dea7e87f8f3c641a022f5ad98b39c37959fe880
kiboma <omayioben@gmail.com>
2023-01-12 14:13:36 +0300

    List comprehensions

--- python/list_comprehensions.py
+++ python/list_comprehensions.py
@@ -1,3 +1,15 @@
-Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.'''
+'''
+    Let’s say I give you a list saved in a variable: 
+    a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. 
+    Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
 
 '''
+#Without comprehension
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+even_list=[]
+for i in a:
+    if i%2==0:
+        even_list.append(i)
+
+print(even_list)
+
