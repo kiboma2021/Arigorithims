7ac387c12b8747f738a1ac9944c7c0cda8935938
kiboma <omayioben@gmail.com>
2023-01-12 16:33:02 +0300

    Check if prime number

--- python/prime_number.py
+++ python/prime_number.py
@@ -2,11 +2,26 @@
 Ask the user for a number and determine whether the number is prime or not. 
 
 '''
-
+import math
 get_number =int(input("Enter a number: "))
 count=0
 for i in range (2,get_number):
     if get_number%i == 0:
         count+=1
+        break
+if count>0:
+    print("The entered number is not a prime number")
+else:
+    print("The entered number is a prime number") 
 
-print("The entered number is not a prime number")
+
+count=0
+sqrt_number=int(math.sqrt(get_number))+1
+for i in range (2,sqrt_number):
+    if get_number%i == 0:
+        count+=1
+        break
+if count>0:
+    print("The entered number is not a prime number")
+else:
+    print("The entered number is a prime number") 
