ff1c093360c463c63872250305722893ae2ceba9
kiboma <omayioben@gmail.com>
2023-01-17 08:20:50 +0300

    Reverse a string

--- python/reverse_array.py
+++ python/reverse_array.py
@@ -4,9 +4,11 @@ output = [c,l,o,s,e,d,' ',s,c,h,o,o,l,' ',c,o,d,i,n,g]
 
 '''
 def reverseArray(arr):
-    n=len(arr)-1
-    for i in range(n,0,-1):
-        print(arr[i])
+    arr=''.join(arr)
+    print(arr)
 
-arr=[c,o,d,i,n,g,' ',s,c,h,o,o,l,' ',c,l,o,s,e,d]
-reverseArray('[c,o,d,i,n,g,' ',s,c,h,o,o,l,' ',c,l,o,s,e,d]')
+    arr=arr.split(' ')
+    print(arr)
+
+arr=['c','o','d','i','n','g',' ','s','c','h','o','o','l',' ','c','l','o','s','e','d']
+reverseArray(arr)
