bbe5e7c2bcb0d463e2dd488f668c3a4d7bb42d6f
kiboma2021 <omayioben@gmail.com>
2023-01-18 19:54:25 +0300

    Test challenge

--- python/test.py
+++ python/test.py
@@ -1,40 +1,20 @@
-str1="We GoNNA Go to amErica"
-print(str1.lower())
-print(str1.upper())
+def diagonalDifference(arr):
+    # Write your code here
+    primary=0
+    secondary=0
+    n=len(arr)
+    print(n)
+    
+    for i in range(0,n):
+        print("====",arr[i][n-1-i])
+        primary+=arr[i][i]
+        secondary+=arr[i][n-1-i]
+    diagonaldif=abs(abs(primary)-abs(secondary))
+    return diagonaldif #diagonaldif
 
-print(str1.split(" "))
+a = [[ 1, 2, 3, -4 ],
+     [ 5, 6, 7, 8 ],
+     [ 1, 2, 3, 4 ],
+     [ -5, 6, 7, 8 ]]
 
-
-print("================================================")
-str1=str1.lower()
-str2=str1.split(" ")
-
-print(str1)
-print(str2)
-
-str3=' '.join(str2)
-print(str3)
-print("================================================")
-
-#This loops through each character
-for i in str1:
-    print(i,end=" ")
-print("\n")
-
-#This loops through each word
-for i in str2:
-    print(i,end=" ")
-print("\n")
-
-
-print("===================Reverse all characters=============================")
-
-for i in range(len(str1)-1,-1,-1):
-    print(str1[i],end="")
-print("\n")
-
-print("===================Reverse each word =============================")
-
-for i in range(len(str2)-1,-1,-1):
-    print(str2[i],end=" ")
-print("\n")
+print(diagonalDifference(a))
