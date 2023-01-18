776c9c1a565a89abd8d2cced3a68d05724170430
kiboma2021 <omayioben@gmail.com>
2023-01-18 13:17:56 +0300

    Use str in python classes

--- python/classes3.py
+++ python/classes3.py
@@ -1,14 +1,15 @@
 '''
 Create a class named Person, use the __init__() function to assign values for name and age:
+use __str__
 
 '''
-
 class Person:
-    def __init__(self, name, age):
+    def __init__(self,name,age):
         self.name=name
         self.age=age
     
-p1=Person("james",36)
-print(p1)
-print(p1.name)
-print(p1.age)
+    def __str__(self):
+        return f"{self.name}({self.age})"
+    
+P1=Person("Mathew",45)
+print(P1)
