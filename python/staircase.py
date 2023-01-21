a6c7c68980655de1612b8490126e781381ce03b6
kiboma2021 <omayioben@gmail.com>
2023-01-21 12:54:44 +0300

    Solve grading system

--- python/staircase.py
+++ python/staircase.py
@@ -10,11 +10,12 @@ The last line is not preceded by any spaces.
 Write a program that prints a staircase of size .
 
 '''
+
 def staircase(n):
     # Write your code here
-    for i in range(0,n):
-        for j in range(0,i+1):
-            print("#",end=" ")
-        print("\n")
+    for i in range(1,n):
+        print(' '*(n-1-i),"#"*i)
+    print("#"*n)
 
-staircase(4)
+
+staircase(20)
--- python/test3.py
+++ python/test3.py
@@ -1,18 +1,7 @@
-def timeConversion(s):
+def staircase(n):
     # Write your code here
-    mil_hrs={'12':'12','01':'13','02':'14','03':'15','04':'16','05':'17',
-    '06':'18','07':'19','08':'20','09':'21','10':'22','11':'23'}
-    if 'AM' in s:
-        if s[0:2]=='12':
-            s=s.replace(s[0:2],'00')
-        military_hr=s.replace('AM',"")
-    else:
-        m=s[0:2]
-        print(m)
-        print(mil_hrs['02'])
-        s=s.replace(s[0:2],mil_hrs[s[0:2]])
-        military_hr=s.replace('PM',"")
+    for i in range(1,n):
+        print(' ' * (n-1-i), "#" * i)
+    print("#" * n)
 
-    return military_hr
-
-print(timeConversion('03:05:45PM'))
+staircase(4)
