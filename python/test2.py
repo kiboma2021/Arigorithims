58caa8f2450f49bc532eb0a04451ce1c00f8cbca
kiboma2021 <omayioben@gmail.com>
2023-01-25 08:32:51 +0300

    Solve strange game challenge

--- python/test2.py
+++ python/test2.py
@@ -1,7 +1,17 @@
 """
-+ - Records a new score that is the sum of the previous two scores
-D - Records a new score that is double the previous score
-C - Invalidates the previous score, removing it from the record
+You are keeping the scores for a baseball game with strange rules. At the beginning of the game, you start with an empty record.
+
+You are given a list of strings operations, where operations[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x.
+Record a new score of x.
+'+'.
+Record a new score that is the sum of the previous two scores.
+'D'.
+Record a new score that is the double of the previous score.
+'C'.
+Invalidate the previous score, removing it from the record.
+Return the sum of all the scores on the record after applying all the operations.
 
 """
 ops=['5','2','C','D','+']
