c7e6c73c5e20bfe0b2ac5792687851c104103fdc
kiboma2021 <omayioben@gmail.com>
2023-01-27 17:52:48 +0300

    Get highest non-duplicated num

--- python/test3.py
+++ python/test3.py
@@ -1,22 +1,11 @@
-def moseCordeconv(morsecode):
-    morsecode=tuple(morsecode)
-    result=[]
-    n=len(morsecode)
-    for j in range(1,n):
-        i=j
-        updatedmorsecode=list(morsecode)
-        while i<n:
-            if updatedmorsecode[i]=='.' and updatedmorsecode[i-1]==".":
-                updatedmorsecode[i]='-'
-                updatedmorsecode[i-1]='-'
-                i+=3
-            else:
-                i+=1
-
-        updatedmorsecode=''.join(updatedmorsecode)
-        result.append(updatedmorsecode)
-
-    print("=======",result)
-
-morsecode="...."
-moseCordeconv(morsecode)
+cards=[[5,7,5,8,8,4,9,9],[5,6,4,5,6,2,4],[6,5,6,5,4,2,5,8,8,9,9]]
+highest_num=0
+for card in cards:
+    #card=sorted(card)
+    for i in card:
+        if i>highest_num and card.count(i)==1:
+            highest_num=i
+if highest_num==0:
+    print(-1)
+else:
+    print(highest_num)
