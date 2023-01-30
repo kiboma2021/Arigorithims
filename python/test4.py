caecd90f61e437744a5bfdaf8d8dbbee0c254c87
kiboma2021 <omayioben@gmail.com>
2023-01-30 09:06:30 +0300

    Calculate power sets

---
+++ python/test4.py
@@ -0,0 +1,7 @@
+def powerset(s):
+    x = len(s)
+    masks = [1 << i for i in range(x)]
+    for i in range(1 << x):
+        yield [ss for mask, ss in zip(masks, s) if i & mask]
+
+print(list(powerset([4, 5, 6])))
